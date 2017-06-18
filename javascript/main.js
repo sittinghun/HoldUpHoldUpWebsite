f2287f3fd409a072ac7dd0563abb8508d7d0d44d
Atthar Mirza <atthar.mirza@gmail.com>
2017-06-17 22:05:10 -0400

    Added Background Pipes and Play/Pause Buttons
    The site is basically complete, just fine tuning at this point.

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function () {
+
     $(window).resize(function () {
         location.reload();
     })
+
+    $('#pause').hide();
+
+    var song = document.getElementById('forbiddenGreens')
+
+    $('#play').click(function () {
+        $(this).hide();
+        $('#pause').show();
+        song.play();
+    });
+
+    $('#pause').click(function () {
+        $(this).hide();
+        $('#play').show();
+        song.pause();
+    });
+
 });
